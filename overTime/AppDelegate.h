ccd70307f16c775224707003a3c7d3e7e68a6caf
Richard Wang <Richard@RichardMBP.local>
2016-07-26 23:04:09 +0800

    overTime文件夹下的文件

---
+++ overTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  overTime
+//
+//  Created by Richard Wang on 16/7/26.
+//  Copyright © 2016年 Richard Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ overTime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  overTime
+//
+//  Created by Richard Wang on 16/7/26.
+//  Copyright © 2016年 Richard Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
