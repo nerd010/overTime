511defce6074de60c88a047a514eac24f77ce456
Richard Wang <Richard@RichardMBP.local>
2016-07-27 00:17:21 +0800

    expand

---
+++ overTime/Expand/category/NSString+CompareDate.h
@@ -0,0 +1,15 @@
+//
+//  NSString+CompareDate.h
+//  overtime
+//
+//  Created by Charles Wang on 15-11-8.
+//  Copyright (c) 2015年 CharlesWang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (CompareDate)
+
++ (NSString *)compareCurrentTime:(NSDate*) compareDate;
+
+@end
---
+++ overTime/Expand/category/NSString+CompareDate.m
@@ -0,0 +1,46 @@
+//
+//  NSString+CompareDate.m
+//  overtime
+//
+//  Created by Charles Wang on 15-11-8.
+//  Copyright (c) 2015年 CharlesWang. All rights reserved.
+//
+
+#import "NSString+CompareDate.h"
+
+@implementation NSString (CompareDate)
+
++ (NSString *)compareCurrentTime:(NSDate*) compareDate
+{
+    NSTimeInterval  timeInterval = [compareDate timeIntervalSinceNow];
+    timeInterval = -timeInterval;
+    long temp = 0;
+    NSString *result;
+    if (timeInterval < 60) {
+        result = [NSString stringWithFormat:@"work for %ld seconds",temp];
+    }
+    else if((temp = timeInterval/60) <60){
+        
+        result = [NSString stringWithFormat:@"work for %ld minute",temp];
+    }
+    
+    else if((temp = temp/60) <24){
+        result = [NSString stringWithFormat:@"%ld",temp];
+    }
+    
+    else if((temp = temp/24) <30){
+        result = [NSString stringWithFormat:@"%ld",temp];
+    }
+    
+    else if((temp = temp/30) <12){
+        result = [NSString stringWithFormat:@"%ld",temp];
+    }
+    else{
+        temp = temp/12;
+        result = [NSString stringWithFormat:@"%ld",temp];
+    }
+    
+    return  result;
+}
+
+@end
