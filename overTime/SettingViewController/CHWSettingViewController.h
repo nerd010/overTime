5968460392240278289ea410b97fcc5b919343f6
Richard Wang <Richard@RichardMBP.local>
2016-07-27 00:20:17 +0800

    settingViewController

---
+++ overTime/SettingViewController/CHWSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHWSettingViewController.h
+//  overtime
+//
+//  Created by Charles Wang on 15-9-29.
+//  Copyright (c) 2015年 CharlesWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHWSettingViewController : UIViewController
+
+@end
---
+++ overTime/SettingViewController/CHWSettingViewController.m
@@ -0,0 +1,38 @@
+//
+//  CHWSettingViewController.m
+//  overtime
+//
+//  Created by Charles Wang on 15-9-29.
+//  Copyright (c) 2015年 CharlesWang. All rights reserved.
+//
+
+#import "CHWSettingViewController.h"
+
+@interface CHWSettingViewController ()
+
+@end
+
+@implementation CHWSettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"设置";
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
