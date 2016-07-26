85dc0d0884263d67ce0e2ef3a5d0fe6eacdde1d9
Richard Wang <Richard@RichardMBP.local>
2016-07-27 00:31:44 +0800

    WorkoffViewController

---
+++ overTime/WorkoffViewController/CHWWorkOffViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHWWorkOffViewController.h
+//  overtime
+//
+//  Created by Charles Wang on 15-9-30.
+//  Copyright (c) 2015年 CharlesWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHWWorkOffViewController : UIViewController
+
+@end
---
+++ overTime/WorkoffViewController/CHWWorkOffViewController.m
@@ -0,0 +1,37 @@
+//
+//  CHWWorkOffViewController.m
+//  overtime
+//
+//  Created by Charles Wang on 15-9-30.
+//  Copyright (c) 2015年 CharlesWang. All rights reserved.
+//
+
+#import "CHWWorkOffViewController.h"
+
+@interface CHWWorkOffViewController ()
+
+@end
+
+@implementation CHWWorkOffViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
